1203e0a937ae4acbb64d8493054268a7eb1d5b21
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-08-09 09:44:07 -0400

    Update GettingFullActions.py

--- GettingFullActions.py
+++ GettingFullActions.py
@@ -4,7 +4,7 @@ import os
 
 def UpdatingActions():
     # Path to the folder containing CSV action files
-    folder_path = 'PostMatchReviewApp_v2/Actions PSD'
+    folder_path = 'Actions PSD'
 
     # Find all CSV files in the folder
     csv_files = glob.glob(os.path.join(folder_path, '*.csv'))
