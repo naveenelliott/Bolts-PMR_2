2715839e10aea4f2dce0ce945d00e7c9e26de7a7
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-08-09 10:04:57 -0400

    Update GKGradeStreamlit.py

--- GKGradeStreamlit.py
+++ GKGradeStreamlit.py
@@ -164,7 +164,7 @@ def GKMoreDetailedFunction(dataframe):
 
         date = more_data['Match Date']
 
-        gk_df = pd.read_csv("PostMatchReviewApp_v3/Thresholds/GoalkeeperThresholds.csv")
+        gk_df = pd.read_csv("Thresholds/GoalkeeperThresholds.csv")
         
         
         if (total_def_actions != 0):
