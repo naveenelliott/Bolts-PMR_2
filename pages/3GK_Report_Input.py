e69ab663827bba891812b75411a1049f9875a243
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-08-19 16:20:00 -0400

    Update 3GK_Report_Input.py

--- pages/3GK_Report_Input.py
+++ pages/3GK_Report_Input.py
@@ -4,6 +4,7 @@ import numpy as np
 from streamlit_gsheets import GSheetsConnection
 
 st.title("Goalkeeper Report Input File")
+st.set_page_config(page_title='Bolts Post-Match Review App', page_icon = 'pages/Boston_Bolts.png')
 
 gk_data = st.session_state['gk_df']
 gks = list(gk_data['Player Full Name'].unique())
